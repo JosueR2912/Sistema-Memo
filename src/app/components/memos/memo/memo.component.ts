c5c24f58ae517588f31604d888f2f8d63b21ccd6
Josue Ramirez <130937012+JosueR2912@users.noreply.github.com>
2025-08-29 09:52:58 -0400

    upload img

--- src/app/components/memos/memo/memo.component.ts
+++ src/app/components/memos/memo/memo.component.ts
@@ -45,7 +45,6 @@ export class MemoComponent implements OnInit {
 
   async init() {
       const select = await this.server.getMemo(this.id);
-      console.log(select);
       if (select?.response) {
         this.body = select.response[0];
         console.log(this.body);
@@ -71,38 +70,98 @@ export class MemoComponent implements OnInit {
     const cargo = this.cargos.find(x => x.id === aux?.id_cargo);
     return cargo?.nombre;
   }
+  getProvidencia(id:number):any{
+    return this.users.find(x => x.id == id)?.providencia;
+  }
 
   async generatePDF() {
-    const doc = new jsPDF();
-    doc.addImage("../../../../assets/img/LOGOS CORPOLARA 2.png", 'PNG', 25.4, 10, 55, 10);
+    const doc = new jsPDF({
+      format:"letter"
+    });
+    doc.addImage("../../../../assets/img/CINTILLO SUPERIOR - HOJA MEMBRETADA.png", 'PNG', 10, 5, 200, 15);
     doc.setFontSize(15);
     doc.setFont("Times", "bold");
     doc.text("M E M O R A N D O", 83, 30);
     doc.setFontSize(12);
     doc.text('N° ' + this.body.codigo_memo,25.4, 40);
     let aux = new Date(this.body.fecha);
-    if (this.body.copia_para !== 0) {
+    if (this.body.copia_para !== 0 && this.getCopias()!= "undefined") {
       doc.text('Para: ' + this.getdepartamento(this.body.toDepartamento),25.4, 45);
-      doc.text('Copia para: ' + this.getdepartamento(this.body.copia_para),25.4, 50);
-      doc.text( 'De: ' + this.getdepartamento(this.body.fromDepartamento),25.4, 55);
-      doc.text('Fecha: ' + aux.getDate() + '/' + (aux.getMonth() + 1) + '/' + aux.getFullYear(),25.4, 60);
-      doc.text('Asunto: ' + this.body.asunto,25.4, 65);
+      let ymain = 45;
+      let copias = doc.splitTextToSize('Copia para: ' + this.getCopias(), doc.internal.pageSize.width - 48);
+      for(let i = 0; i < copias.length; i++){
+        ymain += 5;
+        doc.text(copias[i],25.4,ymain);
+      }
+      //doc.text(copias,25.4, 50);
+      doc.text( 'De: ' + this.getdepartamento(this.body.fromDepartamento),25.4, ymain + 5);
+      doc.text('Fecha: ' + aux.getDate() + '/' + (aux.getMonth() + 1) + '/' + aux.getFullYear(),25.4, ymain + 10);
+      doc.text('Asunto: ' + this.body.asunto,25.4, ymain + 15);
       doc.setFont("Times", "normal");
-      doc.text(this.body.contenido, 30,75);
+      let lines = doc.splitTextToSize(this.body.contenido, doc.internal.pageSize.width - 48);
+      let y = ymain + 25;
+    // for(let i = 0; i < lines.length; i++){
+    //   if(y > 180){
+    //     doc.addImage("../../../../assets/img/LOGOS CORPOLARA 2.png", 'PNG', 25.4, 10, 55, 10);
+    //     doc.addImage("../../../../assets/img/footerMemo.png", 'PNG', 5, 245, 205, 35);
+    //     doc.addPage();
+    //     y = 50;
+    //   }
+    //   doc.text(lines[i], 30, y);
+    //   y += 5;}
+    await this.formatText(lines, doc, y);
     } else{
-      doc.text('Para: ' + this.getdepartamento(this.body.toDepartamento),25.4, 45);
+    doc.text('Para: ' + this.getdepartamento(this.body.toDepartamento),25.4, 45);
     doc.text( 'De: ' + this.getdepartamento(this.body.fromDepartamento),25.4, 50);
     doc.text('Fecha: ' + aux.getDate() + '/' + (aux.getMonth() + 1) + '/' + aux.getFullYear(),25.4, 55);
     doc.text('Asunto: ' + this.body.asunto,25.4, 60);
     doc.setFont("Times", "normal");
-    doc.text(this.body.contenido, 30,70);
+    let lines = doc.splitTextToSize(this.body.contenido, doc.internal.pageSize.width - 48);
+    let y = 70;
+    // for(let i = 0; i < lines.length; i++){
+    //   if(y > 180){
+    //     doc.addImage("../../../../assets/img/LOGOS CORPOLARA 2.png", 'PNG', 25.4, 10, 55, 10);
+    //     doc.addImage("../../../../assets/img/footerMemo.png", 'PNG', 5, 245, 205, 35);
+    //     doc.addPage();
+    //     y = 50;
+    //   }
+    //   doc.text(lines[i], 30, y);
+    //   y += 5;
+    // }
+    await this.formatText(lines, doc, y);
+    
     }
     doc.setFont("Times", "bold");
-    doc.text("Atentamente", 90, 220);
-    doc.addImage(`${this.getFirma(this.body.id_user)}`, 'PNG', 80, 222, 55, 30);
-    doc.text(`${this.getUser(this.body.id_user)}`, 80, 250);
-    doc.text(`${this.getCargoUser(this.body.id_user)}`, 98, 260);
-    doc.addImage("../../../../assets/img/footerMemo.png", 'PNG', 5, 265, 205, 35);
+    doc.text("Atentamente", 95, 185);
+    doc.addImage(`${this.getFirma(this.body.id_user)}`, 'PNG', 85, 187, 55, 30);
+    let cargo = doc.getTextWidth(`${this.getCargoUser(this.body.id_user)}`);
+    let nombre = doc.getTextWidth(`${this.getUser(this.body.id_user)}`);
+    let xcargo = (doc.internal.pageSize.width - cargo) / 2;
+    let xnombre = (doc.internal.pageSize.width - nombre) / 2;
+    doc.text(`${this.getUser(this.body.id_user)}`, xnombre, 215);
+    doc.text(`${this.getCargoUser(this.body.id_user)}`, xcargo, 220);
+    let lines = doc.splitTextToSize(`Segun Providencia Administrativa ${this.getProvidencia(this.body.id_user)}`, doc.internal.pageSize.width - 80);
+    doc.text(`${this.body.redactadoPor}`, 25.4, 240);
+    doc.setFontSize(8)
+    let y = 225
+    let x= 65
+    if(this.getProvidencia(this.body.id_user) != ''){
+      for(let i = 0;i < lines.length; i++ ){
+        let aux = doc.getTextWidth(lines[i]);
+        let auxX = (doc.internal.pageSize.width - aux) / 2;
+       // if(i == 2){
+        // doc.text(lines[i],x + 23 ,y)
+        //}else{
+         // doc.text(lines[i], x, y);
+        //}
+        doc.text(lines[i], auxX,y);
+        y += 5
+        //x -= 2 
+       }
+    }
+    
+   
+    doc.addImage("../../../../assets/img/CINTILLO 2.png", 'PNG', 0, 245, 220, 35);
     doc.save(`Memo_${this.body.codigo_memo}.pdf`);
   }
 
@@ -119,5 +178,58 @@ export class MemoComponent implements OnInit {
       console.log(select);
     }
   }
+  getCopias(){
+    let aux = this.body.copia_para.split(',');
+    let texto = '';
+    if(aux[0] != "0" || aux[0] != ""){
+      for (const copia of aux) {
+        texto += `${this.getdepartamento(parseInt(copia))}, `;
+      }
+      return texto.slice(0, -2);
+    }
+    return texto
+   
+    
+  }
+
+async formatText(text:string, doc:any, y:number){
+   let lines = doc.splitTextToSize(text, doc.internal.pageSize.width - 48);
+  let textAux: string[] = [''];
+  let auxCountline = 0;
+  let countPage = 0;
+  const initialY = y; // Guarda la posición Y inicial
+
+  for (let i = 0; i < lines.length; i++) {
+    if (y < 180) {
+      textAux[countPage] += lines[auxCountline]; // Agrega una nueva línea
+      auxCountline++;
+    } else {
+      // Agrega imágenes antes de cambiar de página
+      doc.addImage("../../../../assets/img/CINTILLO 2.png", 'PNG', 0, 245, 220, 35);
+      doc.addImage("../../../../assets/img/CINTILLO SUPERIOR - HOJA MEMBRETADA.png", 'PNG', 10, 5, 200, 15);
+      doc.addPage();
+
+      // Incrementa el contador de páginas
+      countPage++;
+      y = initialY; // Reinicia la posición Y al comienzo de la nueva página
+      textAux[countPage] = ''; // Inicia una nueva línea para la nueva página
+      textAux[countPage] += lines[auxCountline]; // Agrega la línea actual
+      auxCountline++;
+    }
+    y += 5; // Incrementa la posición Y
+  }
+
+  // Justificar el texto en cada página
+  for (let i = 0; i <= countPage; i++) {
+    doc.setPage(i + 1); // Cambia a la página correspondiente
+    // Reinicia Y al valor inicial para cada página
+    let currentY = initialY;
+    if(i > 0 ){
+      currentY = 30;
+    } 
+    doc.text(textAux[i], 30, currentY, { align: 'justify', maxWidth: doc.internal.pageSize.width - 48, lineHeightFactor: 1.5 });
+    currentY += 5 * textAux[i].split('\n').length; // Actualiza la posición Y para la próxima línea si es necesario
+  }
+}
 
 }
